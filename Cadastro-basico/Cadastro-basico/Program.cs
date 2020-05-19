44e46e6e7032439e28d88d42d236049117f3473f
Charles Willian <lendwy@live.com>
2020-05-19 19:48:44 -0300

    Primeiro.

---
+++ Cadastro-basico/Cadastro-basico/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Cadastro_basico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nome; //String = letras e caracteres
+            String sobrenome;
+            double idade; //Usado para numeros. (Lembrar de estudar mais o double)
+
+            Console.WriteLine("Qual o seu nome ?");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("E o seu sobrenome ?");
+            sobrenome = Console.ReadLine();
+
+            Console.WriteLine("E a sua idade ?");
+            idade = Convert.ToDouble(Console.ReadLine());
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
