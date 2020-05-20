b1db24b63da795c21042fccaddbcf9a6cc612439
Charles Willian <lendwy@live.com>
2020-05-20 16:46:06 -0300

    perguntas

--- Cadastro-basico/Cadastro-basico/Program.cs
+++ Cadastro-basico/Cadastro-basico/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32.SafeHandles;
+using System;
 
 namespace Cadastro_basico
 {
@@ -7,18 +8,29 @@ namespace Cadastro_basico
         static void Main(string[] args)
         {
             String nome; //String = letras e caracteres
-            String sobrenome;
+           // String sobrenome; 
             double idade; //Usado para numeros. (Lembrar de estudar mais o double)
 
             Console.WriteLine("Qual o seu nome ?");
             nome = Console.ReadLine();
 
-            Console.WriteLine("E o seu sobrenome ?");
-            sobrenome = Console.ReadLine();
+            //Console.WriteLine("E o seu sobrenome ?");
+            //sobrenome = Console.ReadLine();
 
             Console.WriteLine("E a sua idade ?");
             idade = Convert.ToDouble(Console.ReadLine());
 
+            Console.WriteLine("Olá Senhor "+nome);
+
+            if (idade >= 18)
+            {
+                Console.WriteLine("Maioridade confirmada");
+            }
+
+            if (idade <= 17)
+            {
+                Console.WriteLine("Ainda de menor");
+            }
 
 
             Console.ReadKey();
